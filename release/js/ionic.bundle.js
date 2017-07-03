d2b281c4271c1ff4ccaf8c6efde87ff00e151da5
alexb <boukhlif.alexandre@gmail.com>
2017-07-03 14:54:30 -0300

    Release

--- release/js/ionic.bundle.js
+++ release/js/ionic.bundle.js
@@ -734,7 +734,7 @@ window.ionic.version = '1.3.2';
     // whatever lookup was done to find this element failed to find it
     // so we can't listen for events on it.
     if(element === null) {
-      void 0;
+      console.error('Null element passed to gesture (element does not exist). Not listening for gesture');
       return this;
     }
 
@@ -2460,7 +2460,9 @@ window.ionic.version = '1.3.2';
   function verifyPlatformReady() {
     setTimeout(function() {
       if(!self.isReady && self.isWebView()) {
-        void 0;
+        console.warn('Possible issue: deviceready did not fire in a reasonable amount of time. ' +
+        'This can be caused by plugins in an inconsistent state. One possible solution: uninstall/remove all ' +
+        'plugins and reinstall them. Additionally, one or more plugins might be faulty or out of date.');
       }
     }, platformReadyTimer);
   }
@@ -2964,7 +2966,12 @@ function tapClickGateKeeper(e) {
   if ((ionic.scroll.isScrolling && ionic.tap.containsOrIsTextInput(e.target)) ||
       (!e.isIonicTap && !ionic.tap.requiresNativeClick(e.target))) {
     //console.log('clickPrevent', e.target.tagName);
-    e.stopPropagation();
+
+    // ADDED by LeSondier: calling stopPropagation on audio click breaks Safari
+    // audio play/pause controllers
+    if (!(e.target.tagName == 'AUDIO' && ionic.Platform.platform() == 'macintel')) {
+        e.stopPropagation();
+    }
 
     if (!ionic.tap.isLabelWithTextInput(e.target)) {
       // labels clicks from native should not preventDefault othersize keyboard will not show on input focus
@@ -6991,7 +6998,7 @@ ionic.scroll = {
 (function(ionic) {
   var NOOP = function() {};
   var deprecated = function(name) {
-    void 0;
+    console.error('Method not available in native scrolling: ' + name);
   };
   ionic.views.ScrollNative = ionic.views.View.inherit({
 
